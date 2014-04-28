96e3af4a86b7c96abcc6e805b938a3351833875b
Chris Camplin <chriscamplin@me.com>
2014-04-28 15:46:06 +0100

    initial commit - a biggy

---
+++ contact/contact-header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title> : : F E R N A N D O : : S O L I S : : C O N T A C T : : </title>
+<link rel="stylesheet" href="../css/global_horiz.css" rel="stylesheet" type="text/css" />
+<script src="../js/3d/prototype.js" type="text/javascript" charset="utf-8"></script>
+<script src="../js/3d/scriptaculous.js" type="text/javascript" charset="utf-8"></script>
+<script src="../js/fade.js" type="text/javascript" charset="utf-8"></script>
+</head>
+<body class="horizontal contact">
+<div id="container">
+	 <div id="top">
+		<div id="header">
+			<a class="index" href="../index.html"> <h1>Fernando Solis</h1></a>
+			<a href="http://www.facebook.com/fernando.solis1"><img src="../i/facebook_16.png" width="16" height="16" alt="Facebook 16"></a>
+			<a href="http://twitter.com/#!/fernandosolis"><img src="../i/twitter_16.png" width="16" height="16" alt="Twitter 16"></a>
+		</div>
+		<ul id="VerColMenu">
+			<li><a title="Click to open or close this section" href="../about.html" class="about">About</a></li>
+			<li><a title="Click to open or close this section" href="../products.html" class="product">Product Design</a></li>
+			<li><a title="Click to open or close this section" href="../fashion.html" class="fashion">Fashion Design</a></li>
+			<li><a title="Click to open or close this section" href="../graphics.html" class="graphic">Graphic Design</a></li>
+			<li><a title="Click to open or close this section" href="http://www.fernandosolis.com/blog/" class="blog">Blog</a> </li>
+			<li><a title="Click to open or close this section" href="../contact/contact.php" class="contact">Contact</a> </li>
+		</ul>
+	</div>
